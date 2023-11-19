c32499e57d0e63cf47447e88cafbc88eb02f5e41
Ignacio Esteban Fonts <ignacio.fonts@banana-software.com>
2023-11-19 00:21:21 -0300

    Mejora de Funciones y graficos

--- binary_signal/lib/main.dart
+++ binary_signal/lib/main.dart
@@ -109,9 +109,8 @@ class _BinarySignalChartState extends State<BinarySignalChart> {
               child: LineChart(
                 LineChartData(
                   titlesData: const FlTitlesData(
-                      topTitles: AxisTitles(
-                        axisNameWidget: Text(''),
-                      ),
+                      bottomTitles: AxisTitles(axisNameWidget: Text('')),
+                      topTitles: AxisTitles(axisNameWidget: Text('')),
                       rightTitles: AxisTitles(axisNameWidget: Text(''))),
                   extraLinesData: ExtraLinesData(
                     horizontalLines: [
@@ -121,11 +120,12 @@ class _BinarySignalChartState extends State<BinarySignalChart> {
                       ),
                     ],
                   ),
+                  
                   lineBarsData: [
                     LineChartBarData(
+                      dotData: const FlDotData(show: false),
                       color: Colors.red,
                       spots: signalDataDefault.asMap().entries.expand((entry) {
-                        // Mapear puntos para mantener una línea horizontal en cambios de bit
                         if (entry.key > 0 &&
                             signalDataDefault[entry.key - 1] != entry.value) {
                           return [
@@ -158,9 +158,8 @@ class _BinarySignalChartState extends State<BinarySignalChart> {
                 child: LineChart(
                   LineChartData(
                     titlesData: const FlTitlesData(
-                        topTitles: AxisTitles(
-                          axisNameWidget: Text(''),
-                        ),
+                        bottomTitles: AxisTitles(axisNameWidget: Text('')),
+                        topTitles: AxisTitles(axisNameWidget: Text('')),
                         rightTitles: AxisTitles(axisNameWidget: Text(''))),
                     extraLinesData: ExtraLinesData(
                       horizontalLines: [
@@ -172,6 +171,7 @@ class _BinarySignalChartState extends State<BinarySignalChart> {
                     ),
                     lineBarsData: [
                       LineChartBarData(
+                        dotData: const FlDotData(show: false),
                         color: Colors.red,
                         spots: signalData.asMap().entries.expand((entry) {
                           // Mapear puntos para mantener una línea horizontal en cambios de bit
@@ -200,12 +200,6 @@ class _BinarySignalChartState extends State<BinarySignalChart> {
     );
   }
 
-  List<double> signalByDefault(String binaryInput, String signalType) {
-    List<int> binaryList = binaryInput.split('').map(int.parse).toList();
-    signalDataDefault = generateNRZSignal(binaryList);
-    return signalDataDefault;
-  }
-
   void generateSignalData(String binaryInput, String signalType) {
     List<int> binaryList = binaryInput.split('').map(int.parse).toList();
     switch (signalType) {
@@ -232,13 +226,17 @@ class _BinarySignalChartState extends State<BinarySignalChart> {
     }
   }
 
+  List<double> signalByDefault(String binaryInput, String signalType) {
+    List<int> binaryList = binaryInput.split('').map(int.parse).toList();
+    signalDataDefault = generateNRZSignal(binaryList);
+    return signalDataDefault;
+  }
+
   List<double> generateNRZSignal(List<int> binaryList) {
     List<double> signal = [];
     for (int i = 0; i < binaryList.length; i++) {
       signal.add(binaryList[i] == 1 ? 1.0 : 0.0);
-      signal.add(binaryList[i] == 1 ? 1.0 : 0.0);
-      if (i + 1 < binaryList.length && binaryList[i] != binaryList[i + 1]) {
-        signal.add(binaryList[i] == 1 ? 1.0 : 0.0);
+      if (i == binaryList.length - 1) {
         signal.add(binaryList[i] == 1 ? 1.0 : 0.0);
       }
     }
@@ -254,13 +252,10 @@ class _BinarySignalChartState extends State<BinarySignalChart> {
     }
 
     for (int i = 0; i < binaryList.length; i++) {
-      if (binaryList[i] == 1) {
-        invert = !invert;
-      }
-      if (invert) {
-        signal.addAll(List.filled(4, -1.0));
-      } else {
-        signal.addAll(List.filled(4, 1.0));
+      if (binaryList[i] == 1) invert = !invert;
+      signal.add(invert ? -1.0 : 1.0);
+      if (i == binaryList.length - 1) {
+        signal.add(invert ? -1.0 : 1.0);
       }
     }
 
@@ -269,26 +264,20 @@ class _BinarySignalChartState extends State<BinarySignalChart> {
 
   List<double> generatePolarRZSignal(List<int> binaryList) {
     List<double> signal = [];
-    for (int bit in binaryList) {
-      if (bit == 1) {
-        signal.addAll([1.0, 1.0, 0.0]);
-      } else {
-        signal.addAll([-1.0, -1.0, 0.0]);
-      }
+    for (int i = 0; i < binaryList.length; i++) {
+      signal.add(binaryList[i] == 1 ? 1.0 : -1.0);
+      signal.add(0.0);
+      if (i == binaryList.length - 1) signal.add(0.0);
     }
     return signal;
   }
 
   List<double> generateManchesterSignal(List<int> binaryList) {
     List<double> signal = [];
-
     for (int i = 0; i < binaryList.length; i++) {
-      // Para bit 0, transición de voltaje de positivo a negativo (de 1 a -1)
-      if (binaryList[i] == 0) {
-        signal.addAll([1.0, -1.0]);
-      } else {
-        // Para bit 1, transición de voltaje de negativo a positivo (de -1 a 1)
-        signal.addAll([-1.0, 1.0]);
+      signal.addAll(binaryList[i] == 0 ? [1.0, -1.0] : [-1.0, 1.0]);
+      if (i == binaryList.length - 1) {
+        signal.add(binaryList[i] == 0 ? 1.0 : -1.0);
       }
     }
     return signal;
@@ -300,19 +289,14 @@ class _BinarySignalChartState extends State<BinarySignalChart> {
 
     for (int i = 0; i < binaryList.length; i++) {
       if (binaryList[i] == 0) {
-        // Transición continua para el bit 0
-        signal.add(positive ? 1.0 : -1.0); // Cambia al voltaje opuesto
+        signal.add(positive ? 1.0 : -1.0);
       } else {
         if (i == 0) signal.add(1.0);
-        // Transición solo a la mitad del intervalo de tiempo para el bit 1
-        positive = !positive; // Invierte el nivel de voltaje a mitad de bit
-        signal.add(
-            positive ? 1.0 : -1.0); // Cambia a voltaje opuesto a mitad de bit
+        positive = !positive;
+        signal.add(positive ? 1.0 : -1.0);
       }
-      // Mantiene el mismo voltaje para la segunda mitad del bit
       signal.add(positive ? -1.0 : 1.0);
     }
-
     return signal;
   }
 
@@ -327,11 +311,9 @@ class _BinarySignalChartState extends State<BinarySignalChart> {
         signal.add(positive ? 1.0 : -1.0);
         positive = !positive;
       }
-      // Añadir un 0 adicional para el último dígito 0 en la cadena
-      if (i == binaryList.length - 1 && binaryList[i] == 0) {
-        signal.add(0.0);
-      }
+      if (i == binaryList.length - 1) signal.add(0.0);
     }
     return signal;
   }
+
 }
